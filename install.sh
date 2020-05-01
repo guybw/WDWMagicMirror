6675b7f3fe3aa2265beef1df04548b9de848d09b
guybw <64648444+guybw@users.noreply.github.com>
2020-05-01 16:44:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
 
+# WDWMagicMirror
+
+First - Install MAgic Mirror with this script 
+
+bash -c "$(curl -sL https://raw.githubusercontent.com/sdetweil/MagicMirror_scripts/master/raspberry.sh)"
+
+bash -c "$(curl -sL https://raw.githubusercontent.com/sdetweil/MagicMirror_scripts/master/screensaveroff.sh)"
+
+bash -c "$(curl -sL https://raw.githubusercontent.com/sdetweil/MagicMirror_scripts/master/fixuppm2.sh)"
+
+# Install HTTtrack
+sudo apt-get install httrack
